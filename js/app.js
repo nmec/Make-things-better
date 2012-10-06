0905f51c9d95b39f89eb7e0773805cc222fe2ed9
Jonathan Warren <jono@nmecdesign.com>
2012-10-06 16:29:52 +0100

    Initial commit.

---
+++ js/app.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+	var vids;
+
+	$.ajax({
+		url: 'js/betterness.json',
+		dataType: 'json',
+		async: false,
+		success: function(data){
+			vids = data;
+		},
+		error: function() {
+			return 'Failed to find betterness :(';
+		}
+	});
+
+	function new_vid() {
+		var betterness = vids.betterness,
+			vidId = betterness[Math.floor(Math.random()*betterness.length)],
+			viewURL = 'https://www.youtube.com/embed/' + vidId + '?rel=0&autoplay=1&controls=0&showinfo=0&iv_load_policy=3&wmode=transparent',
+			viewer = $('#betterness');
+
+		viewer.attr('src', viewURL);
+	}
+
+
+	$('.more').click(function(){
+		new_vid();
+		var wrapper = $('.wrapping');
+		if (wrapper.hasClass('is-visible')) {
+			wrapper
+				.slideUp(500)
+				.slideDown(500);
+		}
+
+		return false;
+	});
+
+	$('.more').one('click', function(){
+		$(this)
+			// .delay(0)
+			.animate({
+				margin: 0
+			}, 500)
+			.removeClass('pulse');
+		$('.wrapping')
+			.slideDown(500)
+			.removeClass('is-hidden')
+			.addClass('is-visible');
+		$('.footer').fadeIn(1000);
+	});
+
+ });
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Make things better with the touch of a button
+
+Help make this project cuter by adding YouTube ID's to `betterness.json`.
